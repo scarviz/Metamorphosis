f5a339fa01762f6046de468ec3205c60c1df2cc7
scarviz <scarviz@gmail.com>
2017-02-25 15:10:37 +0900

    デプロイできるように修正

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		//SceneManager.GetSceneByName("GameSelect");
+		SceneManager.LoadScene("GameSelect", LoadSceneMode.Additive);
+		SceneManager.LoadScene("Player", LoadSceneMode.Additive);
+
+		yield return null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -7,4 +7,7 @@ https://osaka-driven-dev.connpass.com/event/50497/
 |Unity|5.5.1f1|
 |PC|Windows10 x64|
 
-sample test
+|Asset|URL|
+|:--|:--|
+| 250+ Magic Shields FX | https://www.assetstore.unity3d.com/jp/#!/content/38140 |
+
